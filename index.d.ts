e2f6ab247cd2a3f31230c9591c59a1f785f55a8a
John Carmichael <carmichael.john@aol.com>
2019-09-15 13:54:46 +0200

    feat: default input value can be anything
    # 1. First part is one of: chore, docs, feat, fix, refactor, style, or test
    # 2. Epic|Module you are working on: (e.g. AuditLog, REACH, ...)
    # 3. Ticket number followed by a colon: (e.g. BMWCDC-1234)
    # 4. A short (max 50 characters) descriptive message (current time/presence: log known issue, NOT: logged known issue)

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
 export function required(environmentVariable: string): any;
-export function withDefault(environmentVariable: string, defaultValue: string): any;
+export function withDefault(environmentVariable: string, defaultValue: any): any;
